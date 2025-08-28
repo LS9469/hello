bfc82a7765f6156fb96afce8f506a5086a66c619
Logan Shay <lshay2@huskers.unledu>
2025-08-28 08:46:21 -0500

    added my majorto the output

--- hello.c
+++ hello.c
@@ -1,14 +1,17 @@
 /*
+
 Logan Shay
 8/28/25
 
 To print my name*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-    printf("Logan Shay");
+    printf("Hello Worl, I'm Logan Shay\n");
+    prinf("My major is Electrical Engineering!\n");
 
     return 0;
 }
